96ddc860b6fbb8f189ae8e994ff925cde77e1b88
Lei Yang <lei@yangl1996.com>
2021-06-14 15:41:43 -0400

    make the xor function a method

--- playground/code.go
+++ playground/code.go
@@ -2,28 +2,49 @@ package playground
 
 import (
 	"golang.org/x/crypto/blake2b"
+	"hash"
 )
 
 const TxSize = 512
 
-// ProduceCodeword hashes each element with blake2b-256 and XOR those
-// whose hash fall below a certain threshold. Specifically, we pick
-// hashes whose first byte (0-255) is smaller than frac.
-func ProduceCodeword(d [][TxSize]byte, frac int) ([TxSize]byte, error) {
+type HashedTransaction struct {
+	Transaction [TxSize]byte
+	Hash [blake2b.Size256]byte
+}
+
+type TransactionPool struct {
+	Transactions []HashedTransaction
+	hasher hash.Hash
+}
+
+func NewTransactionPool() (*TransactionPool, error) {
+	p := &TransactionPool{}
+	var err error
+	p.hasher, err = blake2b.New256(nil)
+	return p, err
+}
+
+func (p *TransactionPool) AddTransaction(t [TxSize]byte) {
+	p.hasher.Reset()
+	p.hasher.Write(t[:])
+	h := p.hasher.Sum(nil)
+	tx := HashedTransaction{}
+	tx.Transaction = t
+	copy(tx.Hash[:], h)
+	p.Transactions = append(p.Transactions, tx)
+}
+
+// ProduceCodeword selects transactions where the idx-th byte of its hash is
+// less than frac, and XORs them together. idx must be an integer in [0, 32)
+// and frac must be an integer in [0, 256].
+func (p *TransactionPool) ProduceCodeword(idx int, frac int) [TxSize]byte {
 	res := [TxSize]byte{}
-	// get a hasher. we do not want to allocate over and over again
-	hasher, err := blake2b.New256(nil)
-	if err != nil {
-		return res, err
-	}
-	for _, v := range d {
-		hasher.Reset()
-		hasher.Write(v[:])
-		if int(hasher.Sum(nil)[0]) < frac {
+	for _, v := range p.Transactions {
+		if int(v.Hash[idx]) < frac {
 			for i := 0; i < TxSize; i++ {
-				//res[i] = res[i] ^ v[i]
+				res[i] = res[i] ^ v.Transaction[i]
 			}
 		}
 	}
-	return res, nil
+	return res
 }
--- playground/main_test.go
+++ playground/main_test.go
@@ -5,19 +5,27 @@ import (
 	"testing"
 )
 
-func setupData(n int) [][TxSize]byte {
-	data := make([][TxSize]byte, n)
-	for i := 0; i < n; i++ {
-		rand.Read(data[i][:])
+func setupData(n int) (*TransactionPool, error) {
+	p, err := NewTransactionPool()
+	if err != nil {
+		return nil, err
 	}
-	return data
+	for i := 0; i < n; i++ {
+		d := [TxSize]byte{}
+		rand.Read(d[:])
+		p.AddTransaction(d)
+	}
+	return p, nil
 }
 
 func BenchmarkHashing(b *testing.B) {
-	data := setupData(15000)
+	p, err := setupData(15000)
+	if err != nil {
+		b.Fatal(err)
+	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		ProduceCodeword(data, 30)
+		p.ProduceCodeword(2, 30)
 	}
 }
 
