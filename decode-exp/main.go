2d1f9dc7bb01e95859fddf30581d4ceb44673830
Lei Yang <lei@yangl1996.com>
2021-06-15 16:02:23 -0400

    new code

--- decode-exp/main.go
+++ decode-exp/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"time"
 	"math/rand"
+	"errors"
 )
 
 func main() {
@@ -15,6 +16,7 @@ func main() {
 	destSize := flag.Int("d", 10000, "destination pool transaction count")
 	differenceSize := flag.Int("x", 100, "number of transactions that appear in the source but not in the destination")
 	seed := flag.Int64("seed", 0, "seed to use for the RNG, 0 to seed with time")
+	//outputPrefix := flag.String("out", "", "output data path prefix, no output if empty")
 	flag.Parse()
 	var threshold byte
 	if *thresholdInt > 255 || *thresholdInt < 0 {
@@ -24,7 +26,7 @@ func main() {
 		threshold = byte(*thresholdInt)
 	}
 	if *destSize < *srcSize - *differenceSize {
-		fmt.Println("destination pool must be no smaller than source pool minues the difference (d >= s-x)")
+		fmt.Println("destination pool must be no smaller than source pool minus the difference (d >= s-x)")
 		os.Exit(1)
 	}
 	if *seed == 0 {
@@ -33,44 +35,62 @@ func main() {
 		rand.Seed(*seed)
 	}
 
-	p1, err := buildRandomPool(*srcSize)
+	_, err := runExperiment(*srcSize, *destSize, *differenceSize, threshold)
 	if err != nil {
-		fmt.Println("failed to build source pool")
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	p2, err := copyPoolWithDifference(p1, *destSize, *differenceSize)
+
+	return
+}
+
+// runExperiment runs the experiment and returns an array of data. The i-th element in the array is the iteration
+// where the i-th item is decoded.
+func runExperiment(s, d, x int, th byte) ([]int, error) {
+	p1, err := buildRandomPool(s)
 	if err != nil {
-		fmt.Println("failed to build dest pool")
-		os.Exit(2)
+		return nil, err
+	}
+	p2, err := copyPoolWithDifference(p1, d, x)
+	if err != nil {
+		return nil, err
 	}
 
+	res := []int{}
+	res = append(res, 0)	// at iteration 0, we have decoded 0 transactions
 	// start sending codewords from p1 to p2
 	i := 0
+	last := len(p2.Transactions)
 	for ;; {
 		i += 1
 		salt := [4]byte{}	// use 32-bit salt, should be enough
 		rand.Read(salt[:])
-		c := p1.ProduceCodeword(salt[:], threshold)
+		c := p1.ProduceCodeword(salt[:], th)
 		p2.InputCodeword(c)
 		p2.TryDecode()
 		fmt.Printf("Iteration=%v, codewords=%v, transactions=%v\n", i, len(p2.Codewords), len(p2.Transactions))
-		if len(p2.Transactions) == *destSize + *differenceSize {
+		if len(p2.Transactions) > last {
+			for cnt := last; cnt < len(p2.Transactions); cnt++ {
+				res = append(res, i)
+			}
+			last = len(p2.Transactions)
+		}
+		if len(p2.Transactions) == d + x {
 			break
 		}
 	}
 	// compare if p1 is a subset of p2; we take a shortcut by checking if the last differenceSize elements in p2
 	// exist in p1
 	nc := 0
-	for i := *destSize; i < len(p2.Transactions); i++ {
+	for i := d; i < len(p2.Transactions); i++ {
 		if !p1.Exists(p2.Transactions[i].Transaction) {
 			nc += 1
 		}
 	}
 	if nc > 0 {
-		fmt.Println("found", nc, "decoded transactions in p2 that does not appear in p1")
-		os.Exit(1)
+		return nil, errors.New(fmt.Sprint("found", nc, "decoded transactions in p2 that does not appear in p1"))
 	}
-	return
+	return res, nil
 }
 
 func buildRandomPool(n int) (*ldpc.TransactionPool, error) {
