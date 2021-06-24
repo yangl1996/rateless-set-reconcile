0917dbbbf1b652c39f30249112e76aefdc47c02e
Lei Yang <lei@yangl1996.com>
2021-06-24 12:31:33 -0400

    implement hash range

---
+++ ldpc/hashrange.go
@@ -0,0 +1,38 @@
+package ldpc
+
+import (
+	"math"
+)
+
+type HashRange struct {
+	start, end uint64	// both bounds are inclusive
+	cyclic bool
+}
+
+// NewHashRange creates a new HashRange start starts at "start" and covers
+// frac+1 hash values.
+func NewHashRange(start, frac uint64) HashRange {
+	// frac+1 is the number of hash values accepted to this range
+	cyclic := (math.MaxUint64 - start + 1) <= frac
+	var end uint64
+	if cyclic {
+		// from start to MaxUint64: MaxUint64-start+1 hashes
+		// from 0 to end: end+1 hashes
+		// MaxUint64-start+1+end+1 = frac+1
+		// so end = frac - (MaxUint64-start) - 1
+		end = frac - (math.MaxUint64 - start) - 1
+	} else {
+		end = start + frac
+	}
+	return HashRange{start, end, cyclic}
+
+}
+
+// Covers checks if the hash range covers the given hash value.
+func (r *HashRange) Covers(hash uint64) bool {
+	if r.cyclic {
+		return hash >= r.start || hash <= r.end
+	} else {
+		return hash >= r.start && hash <= r.end
+	}
+}
---
+++ ldpc/hashrange_test.go
@@ -0,0 +1,38 @@
+package ldpc
+
+import (
+	"testing"
+	"math"
+)
+
+
+func TestNewHashRange(t *testing.T) {
+	// case 1: no cyclic
+	r1 := NewHashRange(10, 20)
+	c1 := HashRange{10, 30, false}
+	if r1 != c1 {
+		t.Error("incorrect non-cyclic hash range")
+	}
+
+	// case 2: cyclic
+	r2 := NewHashRange(math.MaxUint64-10, 20)
+	c2 := HashRange{math.MaxUint64-10, 9, true}
+	if r2 != c2 {
+		t.Error("incorrect cyclic hash range")
+	}
+}
+
+func TestCovers(t *testing.T) {
+	// case 1: no cyclic
+	r1 := NewHashRange(10, 20)
+	if (!r1.Covers(15)) || r1.Covers(40) {
+		t.Error("incorrect bound check for non-cyclic hash range")
+	}
+
+
+	// case 2: cyclic
+	r2 := NewHashRange(math.MaxUint64-10, 20)
+	if (!r2.Covers(math.MaxUint64)) || r2.Covers(15) {
+		t.Error("incorrect bound check for cyclic hash range")
+	}
+}
