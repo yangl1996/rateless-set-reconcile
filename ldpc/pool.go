084fd6d64a694c68225ddd69d53aa76b804c7e62
Lei Yang <lei@yangl1996.com>
2021-06-24 16:08:37 -0400

    move out

--- ldpc/pool.go
+++ ldpc/pool.go
@@ -28,30 +28,6 @@ func WrapTransaction(t Transaction) HashedTransaction {
 	return tx
 }
 
-var emptySymbol = [TxSize]byte{}
-
-const MaxUintIdx = blake2b.Size/ 8
-
-// Codeword holds a codeword (symbol), its threshold, and its salt.
-type Codeword struct {
-	Symbol [TxSize]byte
-	HashRange
-	Counter int
-	UintIdx int
-}
-
-// ApplyTransaction adds or removes a transaction into/from the codeword.
-// d must have length TxSize, and dir must be Into or From.
-func (c *Codeword) ApplyTransaction(t *Transaction, dir int) {
-	for i:=0; i < TxDataSize/8; i++ {
-		*(*uint64)(unsafe.Pointer(&c.Symbol[i*8])) ^= *(*uint64)(unsafe.Pointer(&t.Data[i*8]))
-	}
-	for i:=0; i < (TxSize-TxDataSize)/8; i++ {
-		*(*uint64)(unsafe.Pointer(&c.Symbol[i*8+TxDataSize])) ^= *(*uint64)(unsafe.Pointer(&t.checksum[i*8]))
-	}
-	c.Counter += dir
-}
-
 // TransactionPool holds the transactions a node has received and validated.
 type TransactionPool struct {
 	Transactions map[HashedTransaction]struct{}
