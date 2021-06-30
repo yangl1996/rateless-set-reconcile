9d1ae3c5481000b542c6891648d7ce1d01939376
Lei Yang <lei@yangl1996.com>
2021-06-30 17:14:12 -0400

    update

--- ldpc/pool.go
+++ ldpc/pool.go
@@ -130,6 +130,7 @@ func (p *TransactionPool) TryDecode() {
 	// try peel the touched transactions off the codewords
 	for cidx, c := range codes {
 		for _, t := range updatedTx {
+			// remove the last condition and we are reverted to the original
 			if _, inc := c.Members[t.Transaction]; c.Covers(&t) && !inc && c.Seq >= p.Transactions[t].FirstAvailable {
 				codes[cidx].PeelTransaction(t.Transaction)
 				change = true
