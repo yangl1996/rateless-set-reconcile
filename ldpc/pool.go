d4b97bb06a03a9d01332e5824001c7da93540f2b
Lei Yang <lei@yangl1996.com>
2021-07-22 14:51:52 -0400

    make trydecode nonrecursive

--- ldpc/pool.go
+++ ldpc/pool.go
@@ -64,7 +64,9 @@ func (p *TransactionPool) AddTransaction(t Transaction) *TimestampedTransaction 
 		return tp
 	}
 	tp := WrapTransaction(t)
-	// BUG: we only need to search for codewords with ps.LastMissing < c.Seq
+	// get a better estimation on the LastMissing timestamp of the tx by
+	// looking at  there
+	// are two cases:
 	// 1. ps.LastMissing >= c.Seq: no need to update anyhow; do not search
 	// 2. ps.LastMissing < c.Seq
 	//   The peer must have not received the transaction at c.Seq. Note that
@@ -81,7 +83,7 @@ func (p *TransactionPool) AddTransaction(t Transaction) *TimestampedTransaction 
 	//
 	//   So, we search backwards in time, and stop at the first c which misses
 	//   tx or when we hit tx.Seq
-	//
+	// BUG: we only need to search for codewords with ps.LastMissing < c.Seq
 	for _, c := range p.ReleasedCodewords {
 		// tx cannot be a member of any codeword in ReleasedCodewords
 		// otherwise, it is already added before the codeword is
@@ -122,10 +124,6 @@ func (p *TransactionPool) MarkCodewordReleased(c *PendingCodeword) {
 		}
 		for _, txv := range p.TransactionTrie.Buckets[c.UintIdx][bi].Items {
 			if c.Covers(&txv.HashedTransaction) {
-				// BUG: This can be saved by updating the txv.FirstAvailable
-				// when we peeled it. (Codewords have pointers to txs,
-				// so they can actually update the time estimations there)
-
 				// Technically, we need to check if txv is a member of the codeword
 				// because we should only update LastMissing of txv when it is NOT
 				// a member of c but is covered by c. However, if txv IS a member,
@@ -171,52 +169,51 @@ func (p *TransactionPool) InputCodeword(c Codeword) {
 // TryDecode recursively peels transactions that we know are members of some codewords,
 // and puts decoded transactions into the pool.
 func (p *TransactionPool) TryDecode() {
-	// scan through the codewords to find ones with counter=1
-	for cidx, _ := range p.Codewords {
-		if p.Codewords[cidx].Counter == 1 {
-			tx := &Transaction{}
-			err := tx.UnmarshalBinary(p.Codewords[cidx].Symbol[:])
-			if err == nil {
-				// store the transaction and peel the c/w, so the c/w is pure
-				tp := p.AddTransaction(*tx)
-				// tp cannot be a candidate; otherwise, it should have been
-				// peeled in ScanCandidate of the last pass
+	change := true
+	for change {
+		change = false
+		// scan through the codewords to find ones with counter=1
+		for cidx, _ := range p.Codewords {
+			if p.Codewords[cidx].Counter == 1 {
+				tx := &Transaction{}
+				err := tx.UnmarshalBinary(p.Codewords[cidx].Symbol[:])
+				if err == nil {
+					// store the transaction and peel the c/w, so the c/w is pure
+					tp := p.AddTransaction(*tx)
+					// tp cannot be a candidate; otherwise, it should have been
+					// peeled in ScanCandidate of the last pass
+					p.Codewords[cidx].PeelTransactionNotCandidate(tp)
+				}
+			}
+		}
+		for cidx, _ := range p.Codewords {
+			// try to speculatively peel
+			// no worry that we are doing redundant work by scanning through all
+			// codewords: SpeculatePeel is smart enough to do nothing if nothing
+			// has changed
+			tx, ok := p.Codewords[cidx].SpeculatePeel()
+			if ok {
+				tp := p.AddTransaction(tx)
 				p.Codewords[cidx].PeelTransactionNotCandidate(tp)
 			}
 		}
-	}
-	for cidx, _ := range p.Codewords {
-		// try to speculatively peel
-		// no worry that we are doing redundant work by scanning through all
-		// codewords: SpeculatePeel is smart enough to do nothing if nothing
-		// has changed
-		tx, ok := p.Codewords[cidx].SpeculatePeel()
-		if ok {
-			tp := p.AddTransaction(tx)
-			p.Codewords[cidx].PeelTransactionNotCandidate(tp)
+		// release codewords and update transaction availability estimation
+		cwIdx := 0	// idx of the cw we are currently working on 
+		for cwIdx < len(p.Codewords) {
+			if p.Codewords[cwIdx].IsPure() {
+				p.MarkCodewordReleased(&p.Codewords[cwIdx])
+				// remove this codeword by moving from the end of the list
+				p.Codewords[cwIdx] = p.Codewords[len(p.Codewords)-1]
+				p.Codewords = p.Codewords[0:len(p.Codewords)-1]
+				change = true
+			} else {
+				cwIdx += 1
+			}
 		}
-	}
-	// release codewords and update transaction availability estimation
-	cwIdx := 0	// idx of the cw we are currently working on 
-	change := false
-	for cwIdx < len(p.Codewords) {
-		if p.Codewords[cwIdx].IsPure() {
-			p.MarkCodewordReleased(&p.Codewords[cwIdx])
-			// remove this codeword by moving from the end of the list
-			p.Codewords[cwIdx] = p.Codewords[len(p.Codewords)-1]
-			p.Codewords = p.Codewords[0:len(p.Codewords)-1]
-			change = true
-		} else {
-			cwIdx += 1
+		// try peel the touched transactions off the codewords
+		for cidx, _ := range p.Codewords {
+			p.Codewords[cidx].ScanCandidates()
 		}
-	}
-	// try peel the touched transactions off the codewords
-	for cidx, _ := range p.Codewords {
-		p.Codewords[cidx].ScanCandidates()
-	}
-	// if any codeword is updated, then we may decode and release more
-	if change {
-		p.TryDecode()
 	}
 }
 
