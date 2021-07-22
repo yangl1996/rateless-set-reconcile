0dfc6fce7fdc1c3a6fde5a18d8d94e2577d6f8e0
Lei Yang <lei@yangl1996.com>
2021-07-22 17:42:03 -0400

    add benchmark support

--- decode-exp/main_test.go
+++ decode-exp/main_test.go
@@ -5,11 +5,35 @@ import (
 	"github.com/yangl1996/rateless-set-reconcile/ldpc"
 )
 
-func BenchmarkExperiment(b *testing.B) {
+func BenchmarkExperiment2000(b *testing.B) {
 	b.ReportAllocs()
-	b.SetBytes(ldpc.TxSize)
-	err := runExperiment(0, 0, 0, 1000, b.N, "p(0.7)", 0, nil, nil, nil, nil, "u(0.01)", 42)
-	if err != nil {
-		b.Fatal(err)
+	b.SetBytes(ldpc.TxSize * 2000)
+	for i := 0; i < b.N; i++ {
+		err := runExperiment(0, 0, 0, 1000, 2000, "p(0.7)", 0, nil, nil, nil, nil, "u(0.01)", 42)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkExperiment5000(b *testing.B) {
+	b.ReportAllocs()
+	b.SetBytes(ldpc.TxSize * 5000)
+	for i := 0; i < b.N; i++ {
+		err := runExperiment(0, 0, 0, 1000, 5000, "p(0.7)", 0, nil, nil, nil, nil, "u(0.01)", 42)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkExperiment20000(b *testing.B) {
+	b.ReportAllocs()
+	b.SetBytes(ldpc.TxSize * 20000)
+	for i := 0; i < b.N; i++ {
+		err := runExperiment(0, 0, 0, 1000, 20000, "p(0.7)", 0, nil, nil, nil, nil, "u(0.01)", 42)
+		if err != nil {
+			b.Fatal(err)
+		}
 	}
 }
