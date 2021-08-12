24a89f18f3fbed55b399534cb9a64b7f4a43f328
Lei Yang <lei@yangl1996.com>
2021-08-12 18:24:29 -0400

    run it for longer

--- analysis/real.sh
+++ analysis/real.sh
@@ -4,7 +4,7 @@ do
 	for ld in {50..90}
 	do
 		l="0.$ld"
-		../decode-exp/decode-exp -f "c($l)" -tc 10000 -l $t -t 100000 -s 1000 -x 1000 &> /dev/null
+		../decode-exp/decode-exp -f "c($l)" -tc 30000 -l $t -t 100000 -s 1000 -x 1000 &> /dev/null
 		first=`sed -n '1500,1500p' out-mean-iter-to-decode.dat`
 		last=`tail -n1 out-mean-iter-to-decode.dat`
 		echo "$l $t $first $last"
