2addda4762e1b2954abb10d5dddd60961e0d6b60
Lei Yang <lei@yangl1996.com>
2021-10-13 19:45:10 -0400

    flexible testbed

--- decode-exp/config.go
+++ decode-exp/config.go
@@ -13,13 +13,11 @@ var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
 var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 var outputPrefix = flag.String("out", "out", "output data path prefix, no output if empty")
 // exp parameters
-var runs = flag.Int("p", 1, "number of parallel runs")
-var commonTx = flag.Int("s", 0, "number of initial common tx at each node")
-var uniqueTx = flag.Int("x", 0, "number of initial unique tx at each node")
-var mirrorProb = flag.Float64("m", 0, "probability that a new transaction arriving at a node appears at all nodes")
+var commonTx = flag.Int("s", 0, "add initial common tx to each node")
+var initialTx = flag.Int("x", 0, "number of initial unique tx at each source")
 var seed = flag.Int64("seed", 0, "seed to use for the RNG, 0 to seed with time")
-var refillTransaction = flag.String("f", "p(0.7)", "transaction arrival pattern at each node: c(r) for uniform arrival at rate r per codeword, p(r) for poisson arrival at rate r, empty string to disable")
-var timeoutDuration = flag.Int("t", 500, "stop the experiment if no new transaction is decoded after this amount of codewords")
+var refillTransaction = flag.String("f", "p(0.7)", "transaction arrival pattern at each source: c(r) for uniform arrival at rate r per codeword, p(r) for poisson arrival at rate r, empty string to disable")
+var timeoutDuration = flag.Int("t", 500, "stop the experiment if no new transaction is decoded after this amount of codewords at any node")
 var timeoutCounter = flag.Int("tc", 0, "number of transactions to decode before stopping, 0 for unlimited")
 var degreeDistString = flag.String("d", "u(0.01)", "distribution of parity check degrees: rs(k,c,delta,diff) for robust soliton with parameters k, c, delta, and estimated diff, s(k,diff) for soliton with parameter k and estimated diff, u(f) for uniform with fraction=f, b(f1, f2, p1) for bimodal with fraction=f1 with probability p1, and fraction=f2 with probability=1-p1")
 var lookbackTime = flag.Uint64("l", 500, "lookback timespan of codewords")
@@ -27,30 +25,38 @@ var readConfig = flag.String("c", "", "read config from `file`; the config will 
 
 func updateConfig(cfg *ExperimentConfig, f *flag.Flag) {
 	switch f.Name {
-	case "p":
-		cfg.ParallelRuns = *runs
 	case "s":
-		cfg.Topology.InitialCommonTx = *commonTx
-	case "x":
-		for sidx := range cfg.Topology.Servers {
-			cfg.Topology.Servers[sidx].InitialUniqueTx = *uniqueTx
+		var ln []string
+		for _, n := range cfg.Topology.Servers {
+			ln = append(ln, n.Name)
 		}
-	case "m":
-		cfg.MirrorProb = *mirrorProb
+		cfg.Topology.Sources = append(cfg.Topology.Sources, Source{"common", "", *commonTx, ln})
+	case "x":
+		for sidx := range cfg.Topology.Sources {
+			cfg.Topology.Sources[sidx].InitialTx= *initialTx
+		}
 	case "seed":
 		cfg.Seed = *seed
 	case "f":
-		for sidx := range cfg.Topology.Servers {
-			cfg.Topology.Servers[sidx].TxArrivePattern = *refillTransaction
+		for sidx := range cfg.Topology.Sources {
+			cfg.Topology.Sources[sidx].ArrivePattern = *refillTransaction
 		}
 	case "t":
-		cfg.TimeoutDuration = *timeoutDuration
+		for sidx := range cfg.Topology.Servers {
+			cfg.Topology.Servers[sidx].TimeoutDuration= *timeoutDuration
+		}
 	case "tc":
-		cfg.TimeoutCounter = *timeoutCounter
+		for sidx := range cfg.Topology.Servers {
+			cfg.Topology.Servers[sidx].TimeoutCounter= *timeoutCounter
+		}
 	case "d":
-		cfg.DegreeDist = *degreeDistString
+		for sidx := range cfg.Topology.Servers {
+			cfg.Topology.Servers[sidx].DegreeDist= *degreeDistString
+		}
 	case "l":
-		cfg.LookbackTime = *lookbackTime
+		for sidx := range cfg.Topology.Servers {
+			cfg.Topology.Servers[sidx].LookbackTime= *lookbackTime
+		}
 	}
 }
 
@@ -68,68 +74,77 @@ func getConfig() (ExperimentConfig, error) {
 		})
 	} else {
 		cfg = ExperimentConfig {
-			MirrorProb: *mirrorProb,
 			Seed: *seed,
-			TimeoutDuration: *timeoutDuration,
-			TimeoutCounter: *timeoutCounter,
-			DegreeDist: *degreeDistString,
-			LookbackTime: *lookbackTime,
-			ParallelRuns: *runs,
 			Topology: Topology {
 				Servers: []Server {
 					Server {
 						Name: "node1",
-						InitialUniqueTx: *uniqueTx,
-						TxArrivePattern: *refillTransaction,
+						TimeoutDuration: *timeoutDuration,
+						TimeoutCounter: *timeoutCounter,
+						DegreeDist: *degreeDistString,
+						LookbackTime: *lookbackTime,
 					},
 					Server {
 						Name: "node2",
-						InitialUniqueTx: *uniqueTx,
-						TxArrivePattern: *refillTransaction,
+						TimeoutDuration: *timeoutDuration,
+						TimeoutCounter: *timeoutCounter,
+						DegreeDist: *degreeDistString,
+						LookbackTime: *lookbackTime,
 					},
 				},
-				InitialCommonTx: *commonTx,
 				Connections: []Connection {
 					Connection {"node1", "node2"},
 				},
+				Sources: []Source {
+					Source {
+						Name: "source1",
+						ArrivePattern: *refillTransaction,
+						InitialTx: *initialTx,
+						Targets: []string{"node1"},
+					},
+					Source {
+						Name: "source2",
+						ArrivePattern: *refillTransaction,
+						InitialTx: *initialTx,
+						Targets: []string{"node2"},
+					},
+					Source {
+						Name: "common",
+						ArrivePattern: "",
+						InitialTx: *commonTx,
+						Targets: []string{"node1", "node2"},
+					},
+				},
 			},
-		}
-	}
-	// validate the dist and pacer strings
-	_, err = NewDistribution(nil, cfg.DegreeDist)
-        if err != nil {
-		return cfg, err
-        }
-	for _, s := range cfg.Topology.Servers {
-		_, err = NewTransactionPacer(nil, s.TxArrivePattern)
-		if err != nil {
-			return cfg, err
 		}
 	}
 	return cfg, nil
 }
 
 type ExperimentConfig struct {
-        MirrorProb            float64	// prob that a tx arriving at a node is mirrored to all other nodes
-        Seed                  int64
-        TimeoutDuration       int	// num of no-decode rounds to wait before exit
-        TimeoutCounter        int	// num of transactions to decode on one node
-        DegreeDist      string
-        LookbackTime          uint64
-	ParallelRuns int
+	Seed                  int64
 	Topology
 }
 
 type Topology struct {
 	Servers []Server
-	InitialCommonTx int
 	Connections []Connection
+	Sources []Source
 }
 
 type Server struct {
 	Name string
-	InitialUniqueTx int
-	TxArrivePattern string
+	TimeoutDuration       int	// num of no-decode rounds to wait before exit
+	TimeoutCounter        int	// num of transactions to decode on one node
+	DegreeDist      string
+	LookbackTime          uint64
+}
+
+type Source struct {
+	Name string
+	ArrivePattern string
+	InitialTx int
+	Targets []string
 }
 
 type Connection struct {
--- decode-exp/node.go
+++ decode-exp/node.go
@@ -6,41 +6,92 @@ import (
 	"github.com/yangl1996/rateless-set-reconcile/ldpc"
 )
 
+type txsource struct {
+	pacer
+	targets []*node
+	rng *rand.Rand
+}
+
+func newSource(cfg Source, rng *rand.Rand, nodes map[string]*node) (*txsource, error) {
+	pc, err := NewTransactionPacer(rng, cfg.ArrivePattern)
+	if err != nil {
+		return nil, err
+	}
+	var targets []*node
+	for _, nodeName := range cfg.Targets {
+		targets = append(targets, nodes[nodeName])
+	}
+	src := &txsource{pc, targets, rng}
+	for i := 0; i < cfg.InitialTx; i++ {
+		tx := src.getRandomTransaction()
+		for _, n := range targets {
+			n.TransactionSync.AddLocalTransaction(tx)
+		}
+	}
+	return src, nil
+}
+
+func (t *txsource) getRandomTransaction() ldpc.Transaction {
+	d := [ldpc.TxDataSize]byte{}
+	t.rng.Read(d[:])
+	return ldpc.NewTransaction(d)
+}
+
+func (t *txsource) generate() int {
+	nadd := t.pacer.tick()
+	for cnt := 0; cnt < nadd; cnt++ {
+		tx := t.getRandomTransaction()
+		for _, target := range t.targets {
+			target.AddLocalTransaction(tx)
+		}
+	}
+	return nadd
+}
+
 type node struct {
 	*ldpc.TransactionSync
 	dist             thresholdPicker
 	rng              *rand.Rand
-	transactionPacer pacer
 	lookback         uint64
 	peers []struct{*node; int}
 	decoded int
 	cwrcvd int
 	lastAct uint64
+	declimit int
+	timeout int
+	name string
 }
 
-func newNode(dist thresholdPicker, rng *rand.Rand, txPacer pacer, lookback uint64) *node {
+func newNode(cfg Server, rng *rand.Rand) (*node, error) {
 	// calculate lookback window, mind overflows
-	txLookback := lookback * 5
-	if txLookback < lookback {
+	txLookback := cfg.LookbackTime * 5
+	if txLookback < cfg.LookbackTime {
 		txLookback = ldpc.MaxTimestamp
 	}
-	cwLookback := lookback * 5
-	if cwLookback < lookback {
+	cwLookback := cfg.LookbackTime * 5
+	if cwLookback < cfg.LookbackTime {
 		cwLookback = ldpc.MaxTimestamp
 	}
-	node := &node{}
-	node.rng = rng
-	node.TransactionSync = &ldpc.TransactionSync{
-		SyncClock: ldpc.SyncClock{
-			TransactionTimeout: txLookback,
-			CodewordTimeout:    cwLookback,
-			Seq:                1,
-		},
+	dist, err := NewDistribution(rng, cfg.DegreeDist)
+	if err != nil {
+		return nil, err
 	}
-	node.dist = dist
-	node.transactionPacer = txPacer
-	node.lookback = lookback
-	return node
+	node := &node{
+		TransactionSync: &ldpc.TransactionSync{
+			SyncClock: ldpc.SyncClock{
+				TransactionTimeout: txLookback,
+				CodewordTimeout:    cwLookback,
+				Seq:                1,
+			},
+		},
+		dist: dist,
+		rng: rng,
+		lookback: cfg.LookbackTime,
+		declimit: cfg.TimeoutCounter,
+		timeout: cfg.TimeoutDuration,
+		name: cfg.Name,
+	}
+	return node, nil
 }
 
 func (n *node) connectTo(peer *node) {
@@ -54,34 +105,6 @@ func (n *node) connectTo(peer *node) {
 		n,
 		len(n.TransactionSync.PeerStates)-1,
 	})
-}
-
-func (n *node) fillInitTransaction(src []ldpc.Transaction, nCopy, nNew int) []ldpc.Transaction {
-	var res []ldpc.Transaction
-	if src != nil {
-		i := 0
-		for _, tx := range src {
-			if i >= nCopy {
-				break
-			}
-			n.TransactionSync.AddLocalTransaction(tx)
-			i += 1
-			res = append(res, tx)
-		}
-	}
-
-	for i := 0; i < nNew; i++ {
-		tx := n.getRandomTransaction()
-		n.TransactionSync.AddLocalTransaction(tx)
-		res = append(res, tx)
-	}
-	return res
-}
-
-func (n *node) getRandomTransaction() ldpc.Transaction {
-	d := [ldpc.TxDataSize]byte{}
-	n.rng.Read(d[:])
-	return ldpc.NewTransaction(d)
 }
 
 func (n *node) sendCodewords() {
