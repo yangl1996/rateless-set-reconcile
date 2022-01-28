7efa4f6b57799a7228cdaa21a3bb695e56ebbc83
Lei Yang <lei@yangl1996.com>
2022-01-28 16:28:37 -0500

    test marking decoded

--- ldpc/pool_test.go
+++ ldpc/pool_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 var hasher hash.Hash64 = siphash.New([]byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f})
+var zeroTx TransactionData = TransactionData{}
 
 func degreeTwoCodeword() (*Transaction, *pendingTransaction, *Transaction, *pendingTransaction, *pendingCodeword) {
     t1 := randomBytes()
@@ -48,8 +49,7 @@ func TestPeelTransaction(t *testing.T) {
 	}
 
 	cw.peelTransaction(tx2stub, tx2)
-	zero := TransactionData{}
-	if cw.symbol != zero {
+	if cw.symbol != zeroTx {
 		t.Error("incorrect result after peeling")
 	}
 	if len(cw.members) != 0 {
@@ -57,4 +57,31 @@ func TestPeelTransaction(t *testing.T) {
 	}
 }
 
+func TestMarkDecoded(t *testing.T) {
+	tx1, tx1stub, tx2, tx2stub, cw1 := degreeTwoCodeword()
+	cw2 := &pendingCodeword{}
+	cw2.symbol.XOR(&tx1.serialized)
+	cw2.members = []*pendingTransaction{tx1stub}
+	tx1stub.blocking = append(tx1stub.blocking, cw2)
 
+	decodable := tx1stub.markDecoded(tx1, nil)
+	if len(decodable) != 2 || decodable[0] != cw1 || decodable[1] != cw2 {
+		t.Error("incorrect list of decodable codewords")
+	}
+	if cw2.symbol != zeroTx {
+		t.Error("incorrect result after peeling")
+	}
+	if len(cw2.members) != 0 {
+		t.Error("incorrect member after peeling")
+	}
+	if cw1.symbol != tx2.serialized {
+		t.Error("incorrect result after peeling")
+	}
+	if len(cw1.members) != 1 || cw1.members[0] != tx2stub {
+		t.Error("incorrect member after peeling")
+	}
+	if !cw1.queued || !cw2.queued {
+		t.Error("codewords not queued")
+	}
+
+}
