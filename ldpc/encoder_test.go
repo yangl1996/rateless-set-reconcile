dd10bd4ffbeb1e088dae5fb4c734d0cf40353f70
Lei Yang <lei@yangl1996.com>
2022-01-28 21:16:50 -0500

    add a decoder

--- ldpc/encoder_test.go
+++ ldpc/encoder_test.go
@@ -59,3 +59,35 @@ func TestEncodeAndDecode(t *testing.T) {
 	}
 	t.Logf("%d codewords until fully decoded", ncw)
 }
+
+func BenchmarkDecode(b *testing.B) {
+	dist := soliton.NewRobustSoliton(rand.New(rand.NewSource(0)), 50, 0.03, 0.5)
+	e := NewEncoder(testSalt, dist, 50)
+	for i := 0; i < 50; i++ {
+		tx, _ := randomTransaction()
+		e.AddTransaction(tx)
+	}
+	// pre-generate 1.35N codewords for N transactions arrival in uniform pattern
+	codewords := make([]*Codeword, 0, b.N + b.N / 3)
+	credit := 0.0
+	for i := 0; i < b.N; i++ {
+		tx, _ := randomTransaction()
+		e.AddTransaction(tx)
+		credit += 1.35
+		for credit > 1.0 {
+			credit -= 1.0
+			cw := e.ProduceCodeword()
+			codewords = append(codewords, cw)
+		}
+	}
+	dec := newPeer(testSalt)
+    b.ReportAllocs()
+    b.SetBytes(TxSize)
+    b.ResetTimer()
+	for _, cw := range codewords {
+		dec.addCodeword(cw)
+	}
+	b.StopTimer()
+	ndec := len(dec.receivedTransactions)
+	b.Logf("decoded %d out of %d (%.2f%%)", ndec, b.N, float64(ndec) / float64(b.N) * 100.0)
+}
