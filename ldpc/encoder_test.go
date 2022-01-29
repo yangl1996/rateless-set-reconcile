a854e1f1361833e84d5b880a27978728d13f638a
Lei Yang <lei@yangl1996.com>
2022-01-28 20:55:23 -0500

    benchmark add transactions

--- ldpc/encoder_test.go
+++ ldpc/encoder_test.go
@@ -17,6 +17,22 @@ func BenchmarkProduceCodeword(b *testing.B) {
     b.SetBytes(TxSize)
     b.ResetTimer()
     for i := 0; i < b.N; i++ {
-        e.ProduceCodeword()
+		e.ProduceCodeword()
+    }
+}
+
+func BenchmarkAddTransaction(b *testing.B) {
+	dist := soliton.NewRobustSoliton(rand.New(rand.NewSource(0)), 50, 0.03, 0.5)
+	e := NewEncoder(testSalt, dist, 50)
+	for i := 0; i < 50; i++ {
+		tx, _ := randomTransaction()
+		e.AddTransaction(tx)
+	}
+	tx, _ := randomTransaction()
+    b.ReportAllocs()
+    b.SetBytes(TxSize)
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+		e.AddTransaction(tx)
     }
 }
