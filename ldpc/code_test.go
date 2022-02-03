df05b07943a99aee319da566356ca7c820b50c75
Lei Yang <lei@yangl1996.com>
2022-02-02 23:15:56 -0500

    test edge slepian wolf

--- ldpc/code_test.go
+++ ldpc/code_test.go
@@ -35,3 +35,37 @@ func TestCodeRate(t *testing.T) {
 		})
 	}
 }
+
+func TestEdgeSlepianWolf(t *testing.T) {
+	step := 0.1
+	for tc := 0.0; tc <= 1.0; tc += step {
+		tc := tc
+		t.Run(fmt.Sprintf("%.2f", tc), func(t *testing.T) {
+			overlap := int(tc * 10000)
+			dist := soliton.NewRobustSoliton(rand.New(rand.NewSource(0)), uint64(10000), 0.03, 0.5)
+			e := NewEncoder(testSalt, dist, 10000)
+			dec := NewPeer(testSalt)
+			ncw := 0
+			for i := 0; i < 10000; i++ {
+				tx, _ := randomTransaction()
+				e.AddTransaction(tx)
+				if i < overlap {
+					dec.AddTransaction(tx)
+					ncw += 1
+				}
+			}
+			for len(dec.receivedTransactions) < 10000 {
+				c := e.ProduceCodeword()
+				dec.AddCodeword(c)
+				ncw += 1
+			}
+			for _, tx := range e.window {
+				_, there := dec.receivedTransactions[tx.saltedHash]
+				if !there {
+					t.Error("missing transaction in the decoder")
+				}
+			}
+			t.Logf("%d codewords (%.2f)", ncw, float64(ncw)/float64(10000))
+		})
+	}
+}
