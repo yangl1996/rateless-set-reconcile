acad37dd84cc8c2b176b9bfeac695793e234f1de
Lei Yang <lei@yangl1996.com>
2022-02-09 21:20:07 -0500

    basically working

--- node/controller.go
+++ node/controller.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-	"github.com/yangl1996/rateless-set-reconcile/ldpc"
 	"encoding/gob"
-	"time"
-	"io"
-	"math/rand"
+	"github.com/yangl1996/rateless-set-reconcile/ldpc"
 	"github.com/yangl1996/soliton"
+	"io"
 	"log"
+	"math/rand"
+	"time"
 )
 
 type sender struct {
-	tx *gob.Encoder
-	encoder *ldpc.Encoder
-	txRate float64	// codeword sending rate in s^-1
+	tx                   *gob.Encoder
+	encoder              *ldpc.Encoder
+	cwRate               float64 // codeword sending rate in s^-1
 	rateIncreaseConstant float64
 	rateDecreaseConstant float64
-	nextCodeword Codeword
+	nextCodeword         Codeword
 
-	sendTimer *time.Timer
-	peerLoss <-chan int
-	ourLoss chan int
-	shutdown chan struct{}
+	sendTimer      *time.Timer
+	peerLoss       <-chan int
+	ourLoss        chan int
+	shutdown       chan struct{}
 	newTransaction chan *ldpc.Transaction
 }
 
 func (s *sender) loop() error {
+	ticker := time.NewTicker(1 * time.Second)
 	log.Println("sender started")
 	for {
 		select {
 		case <-s.shutdown:
 			return nil
 		case l := <-s.peerLoss:
-			s.txRate += s.rateIncreaseConstant * float64(l)
+			s.cwRate += s.rateIncreaseConstant * float64(l)
 		case l := <-s.ourLoss:
 			s.nextCodeword.Loss += l
 		case tx := <-s.newTransaction:
 			s.encoder.AddTransaction(tx)
 		case <-s.sendTimer.C:
 			// schedule the next event
-			s.txRate -= s.rateDecreaseConstant
-			if s.txRate < 1.0 {
-				s.txRate = 1.0
+			s.cwRate -= s.rateDecreaseConstant
+			if s.cwRate < 1.0 {
+				s.cwRate = 1.0
 			}
-			s.sendTimer.Reset(time.Duration(1.0 / s.txRate * float64(time.Second)))
-			log.Println("current rate", s.txRate)
+			s.sendTimer.Reset(time.Duration(1.0 / s.cwRate * float64(time.Second)))
 			// send the codeword
 			s.nextCodeword.Codeword = s.encoder.ProduceCodeword()
 			err := s.tx.Encode(s.nextCodeword)
@@ -52,6 +52,8 @@ func (s *sender) loop() error {
 				return err
 			}
 			s.nextCodeword.Loss = 0
+		case <-ticker.C:
+			log.Println("codeword rate", s.cwRate)
 		}
 	}
 	panic("unreachable")
@@ -68,26 +70,28 @@ type receivedCodeword struct {
 }
 
 type receiver struct {
-	rx *gob.Decoder
+	rx          *gob.Decoder
 	newCodeword chan<- indexedCodeword
-	peerLoss chan<- int
-	peerIdx int
+	peerLoss    chan<- int
+	peerIdx     int
 
-	decoder *ldpc.Decoder
-	rxWindow []receivedCodeword
+	decoder                *ldpc.Decoder
+	rxWindow               []receivedCodeword
 	thirdPartyTransactions []*ldpc.Transaction
-	timeout time.Duration
+	timeout                time.Duration
 }
 
 func (r *receiver) receiveCodeword() error {
-	var cw Codeword
 	for {
-		err := r.rx.Decode(&cw)
+		cw := &Codeword{}
+		err := r.rx.Decode(cw)
 		if err != nil {
 			return err
 		}
 		r.newCodeword <- indexedCodeword{r.peerIdx, cw.Codeword}
-		r.peerLoss <- cw.Loss
+		if cw.Loss > 0 {
+			r.peerLoss <- cw.Loss
+		}
 	}
 }
 
@@ -107,17 +111,17 @@ func (p *peer) ingestTransactions() []*ldpc.Transaction {
 }
 
 type controller struct {
-	newCodeword chan indexedCodeword	// should only be used for receiving
-	peers []peer
-	newPeer chan io.ReadWriter
-	allTransactions []*ldpc.Transaction
+	newCodeword      chan indexedCodeword // should only be used for receiving
+	peers            []*peer
+	newPeer          chan io.ReadWriter
+	allTransactions  []*ldpc.Transaction
 	localTransaction chan *ldpc.Transaction
 }
 
 func newController() *controller {
-	return &controller {
-		newCodeword: make(chan indexedCodeword, 1000),
-		newPeer: make(chan io.ReadWriter),
+	return &controller{
+		newCodeword:      make(chan indexedCodeword, 1000),
+		newPeer:          make(chan io.ReadWriter),
 		localTransaction: make(chan *ldpc.Transaction, 1000),
 	}
 }
@@ -137,39 +141,40 @@ func (c *controller) loop() error {
 			}
 		case conn := <-c.newPeer:
 			peerLoss := make(chan int, 100)
-			r := receiver {
-				rx: gob.NewDecoder(conn),
+			r := receiver{
+				rx:          gob.NewDecoder(conn),
 				newCodeword: c.newCodeword,
-				peerLoss: peerLoss,
-				peerIdx: len(c.peers),
-				decoder: ldpc.NewDecoder(testKey),
-				timeout: time.Duration(0.5 * float64(time.Second)),
+				peerLoss:    peerLoss,
+				peerIdx:     len(c.peers),
+				decoder:     ldpc.NewDecoder(testKey),
+				timeout:     time.Duration(0.5 * float64(time.Second)),
 			}
 			dist := soliton.NewRobustSoliton(rand.New(rand.NewSource(0)), 50, 0.03, 0.5)
-			s := sender {
-				tx: gob.NewEncoder(conn),
-				encoder: ldpc.NewEncoder(testKey, dist, 50),
-				txRate: 1.0,
+			s := sender{
+				tx:                   gob.NewEncoder(conn),
+				encoder:              ldpc.NewEncoder(testKey, dist, 50),
+				cwRate:               1.0,
 				rateIncreaseConstant: 0.1,
 				rateDecreaseConstant: 0.002,
-				sendTimer: time.NewTimer(time.Duration(1.0 / 1.0 * float64(time.Second))),
-				peerLoss: peerLoss,
-				ourLoss: make(chan int, 100),
-				shutdown: make(chan struct{}),
-				newTransaction: make(chan *ldpc.Transaction, 100),
+				sendTimer:            time.NewTimer(time.Duration(1.0 / 1.0 * float64(time.Second))),
+				peerLoss:             peerLoss,
+				ourLoss:              make(chan int, 100),
+				shutdown:             make(chan struct{}),
+				newTransaction:       make(chan *ldpc.Transaction, 100),
 			}
 			for _, existingTx := range c.allTransactions {
 				r.decoder.AddTransaction(existingTx)
 			}
-			c.peers = append(c.peers, peer{s, r})
+			np := &peer{s, r}
+			c.peers = append(c.peers, np)
 			go func() {
-				err := r.receiveCodeword()
+				err := np.receiver.receiveCodeword()
 				if err != nil {
 					panic(err)
 				}
 			}()
 			go func() {
-				err := s.loop()
+				err := np.sender.loop()
 				if err != nil {
 					panic(err)
 				}
@@ -198,7 +203,6 @@ func (c *controller) loop() error {
 			stub, newTx := c.peers[idx].receiver.decoder.AddCodeword(codeword.Codeword)
 			c.peers[idx].receiver.rxWindow = append(c.peers[idx].receiver.rxWindow, receivedCodeword{stub, now})
 			for len(newTx) != 0 {
-				log.Printf("decoded %d transactions\n", len(newTx))
 				// forward the new transactions from the last-decoded node to all peers
 				for idx := range c.peers {
 					if idx != lastDecoded {
@@ -212,6 +216,7 @@ func (c *controller) loop() error {
 				for idx := range c.peers {
 					newTx = c.peers[idx].ingestTransactions()
 					if len(newTx) > 0 {
+						lastDecoded = idx
 						break
 					}
 				}
