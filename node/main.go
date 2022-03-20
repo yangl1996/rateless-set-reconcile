36efaf8eb52d0f9538d86815a7fa4450ba532d22
Lei Yang <lei@yangl1996.com>
2022-03-19 21:34:14 -0400

    update

--- node/main.go
+++ node/main.go
@@ -8,6 +8,8 @@ import (
 	"math/rand"
 	"time"
 	"io"
+	"flag"
+	"strings"
 )
 
 func randomTransaction() *ldpc.Transaction {
@@ -19,6 +21,8 @@ func randomTransaction() *ldpc.Transaction {
 }
 
 func main() {
+	addr := flag.String("l", ":9000", "address to listen")
+	conn := flag.String("c", "", "comma-delimited list of addresses to connect to")
 	newController := func() *controller {
 		return &controller {
 			newPeerConn: make(chan io.ReadWriter),
@@ -26,24 +30,41 @@ func main() {
 			localTransaction: make(chan *ldpc.Transaction, 1000),
 		}
 	}
-	c1 := newController()
-	c2 := newController()
-	go c1.loop()
-	go c2.loop()
-	l, _ := net.Listen("tcp", ":9999")
+	c := newController()
+
+	go c.loop()
+	l, err := net.Listen("tcp", *addr)
+	if err != nil {
+		log.Fatalln("failed to listen to", *addr)
+	}
 	go func() {
-		cn, _ := l.Accept()
-		c1.newPeerConn <- cn
+		for {
+			cn, err := l.Accept()
+			if err != nil {
+				log.Println("error accepting incoming connection")
+			} else {
+				c.newPeerConn <- cn
+			}
+		}
 	}()
-	go func() {
-		cn, _ := net.Dial("tcp", ":9999")
-		c2.newPeerConn <- cn
-	}()
+
+	if *conn != "" {
+		for _, a := range strings.Split(*conn, ",") {
+			go func() {
+				cn, err := net.Dial("tcp", a)
+				if err != nil {
+					log.Println("error connecting to peer", a)
+				} else {
+					c.newPeerConn <- cn
+				}
+			}()
+		}
+	}
 
 	go func() {
 		for {
 			tx := randomTransaction()
-			c1.localTransaction <- tx
+			c.localTransaction <- tx
 			time.Sleep(time.Duration(1 * time.Millisecond))
 		}
 	}()
