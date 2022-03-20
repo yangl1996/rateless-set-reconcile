9207830a3d5d8261fab92acaa79d4cd4e8e3fb6e
Lei Yang <lei@yangl1996.com>
2022-03-19 21:00:44 -0400

    split the files

---
+++ node/receiver.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/gob"
+	"github.com/yangl1996/rateless-set-reconcile/ldpc"
+	"time"
+)
+
+type receivedCodeword struct {
+	*ldpc.PendingCodeword
+	receivedTime time.Time
+}
+
+type receiver struct {
+	rx          *gob.Decoder
+	decoder                *ldpc.Decoder
+	peerLoss    chan<- int
+	ourLoss     chan<- int
+	decodedTransaction chan<- *ldpc.Transaction
+	newTransaction <-chan *ldpc.Transaction
+
+	rxWindow               []receivedCodeword
+	timeout                time.Duration
+}
+
+func (r *receiver) receive(cw chan<- *ldpc.Codeword) error {
+	for {
+		newcw := &Codeword{}
+		err := r.rx.Decode(newcw)
+		if err != nil {
+			return err
+		}
+		if newcw.Loss > 0 {
+			r.peerLoss <- newcw.Loss
+		}
+		cw <- newcw.Codeword
+	}
+}
+
+func (r *receiver) decode(cwChan <-chan *ldpc.Codeword) error {
+	for {
+		select {
+		case cw := <-cwChan:
+			now := time.Now()
+			// clean up the pending codewords
+			loss := 0
+			for len(r.rxWindow) > 0 {
+				head := r.rxWindow[0]
+				dur := now.Sub(head.receivedTime)
+				if dur < r.timeout {
+					break
+				}
+				if !head.Decoded() {
+					loss += 1
+				}
+				head.Free()
+				r.rxWindow = r.rxWindow[1:]
+			}
+			r.ourLoss <- loss
+			// try to decode the new codeword
+			stub, buf := r.decoder.AddCodeword(cw)
+			r.rxWindow = append(r.rxWindow, receivedCodeword{stub, now})
+			for _, ntx := range buf {
+				r.decodedTransaction <- ntx
+			}
+		case tx := <-r.newTransaction:
+			buf := r.decoder.AddTransaction(tx)
+			for _, ntx := range buf {
+				r.decodedTransaction <- ntx
+			}
+		}
+	}
+}
---
+++ node/sender.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/gob"
+	"github.com/yangl1996/rateless-set-reconcile/ldpc"
+	"log"
+	"time"
+)
+
+type sender struct {
+	tx                   *gob.Encoder
+	encoder              *ldpc.Encoder
+	cwRate               float64 // codeword sending rate in s^-1
+	rateIncreaseConstant float64
+	rateDecreaseConstant float64
+	minRate				float64
+	nextCodeword         Codeword
+
+	sendTimer      *time.Timer
+	peerLoss       <-chan int
+	ourLoss        <-chan int
+	newTransaction <-chan *ldpc.Transaction
+}
+
+func (s *sender) loop() error {
+	ticker := time.NewTicker(1 * time.Second)
+	log.Println("sender started")
+	for {
+		select {
+		case l := <-s.peerLoss:
+			s.cwRate += s.rateIncreaseConstant * float64(l)
+		case l := <-s.ourLoss:
+			s.nextCodeword.Loss += l
+		case tx := <-s.newTransaction:
+			s.encoder.AddTransaction(tx)
+		case <-s.sendTimer.C:
+			// schedule the next event
+			s.cwRate -= s.rateDecreaseConstant
+			if s.cwRate < s.minRate {
+				s.cwRate = s.minRate
+			}
+			s.sendTimer.Reset(time.Duration(1.0 / s.cwRate * float64(time.Second)))
+			// send the codeword
+			s.nextCodeword.Codeword = s.encoder.ProduceCodeword()
+			err := s.tx.Encode(s.nextCodeword)
+			if err != nil {
+				return err
+			}
+			s.nextCodeword.Loss = 0
+		case <-ticker.C:
+			log.Println("codeword rate", s.cwRate)
+		}
+	}
+	panic("unreachable")
+}
