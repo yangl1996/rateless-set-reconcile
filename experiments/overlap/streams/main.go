f69a420b11972a28e64d35baac2d43d51607a7ad
Lei Yang <lei@yangl1996.com>
2022-04-11 23:04:47 -0400

    update

--- experiments/overlap/streams/main.go
+++ experiments/overlap/streams/main.go
@@ -8,7 +8,7 @@ import (
 	"math/rand"
 )
 
-func testOverlap(K, N int, overlap float64) {
+func testOverlap(K, N int, overlap, threshold float64) {
 	dist := soliton.NewRobustSoliton(rand.New(rand.NewSource(1)), uint64(K), 0.03, 0.5)
 
 	// create 2N transactions
@@ -24,68 +24,80 @@ func testOverlap(K, N int, overlap float64) {
 		}
 	}
 
-	//minRate := (2.0-overlap)/2.0
-	minRate := 0.0
-	maxRate := 2.0
-	//maxRate := 2.0-overlap
-	for r1 := minRate; r1 <= maxRate; r1 += 0.05 {
-		for r2 := minRate; r2 <= maxRate; r2 += 0.05 {
-			d1 := ldpc.NewDecoder(experiments.TestKey, 2147483647)
-			e1 := ldpc.NewEncoder(experiments.TestKey, dist, K)
-			e2 := ldpc.NewEncoder(experiments.TestKey, dist, K)
+	test := func(r1, r2 float64) (float64, float64) {
+		d1 := ldpc.NewDecoder(experiments.TestKey, 2147483647)
+		e1 := ldpc.NewEncoder(experiments.TestKey, dist, K)
+		e2 := ldpc.NewEncoder(experiments.TestKey, dist, K)
 
-			txset1 := make(map[ldpc.Transaction]struct{})
-			txset2 := make(map[ldpc.Transaction]struct{})
-			c1 := 0.0
-			c2 := 0.0
-			ptr := 0
-			for i := 0; i < N; i++ {
-				if (rand.Float64() < overlap) {
-					e1.AddTransaction(txs[ptr])
-					e2.AddTransaction(txs[ptr])
-					txset1[*txs[ptr]] = struct{}{}
-					txset2[*txs[ptr]] = struct{}{}
-					ptr++
-				} else {
-					e1.AddTransaction(txs[ptr])
-					txset1[*txs[ptr]] = struct{}{}
-					ptr++
-					e2.AddTransaction(txs[ptr])
-					txset2[*txs[ptr]] = struct{}{}
-					ptr++
-				}
-				c1 += r1
-				c2 += r2
-				for c1 >= 1.0 {
-					c := e1.ProduceCodeword()
-					_, newtx := d1.AddCodeword(c)
-					for _, tx := range newtx {
-						delete(txset1, *tx)
-						delete(txset2, *tx)
-					}
-					c1 -= 1.0
-				}
-				for c2 >= 1.0 {
-					c := e2.ProduceCodeword()
-					_, newtx := d1.AddCodeword(c)
-					for _, tx := range newtx {
-						delete(txset1, *tx)
-						delete(txset2, *tx)
-					}
-					c2 -= 1.0
-				}
+		txset1 := make(map[ldpc.Transaction]struct{})
+		txset2 := make(map[ldpc.Transaction]struct{})
+		c1 := 0.0
+		c2 := 0.0
+		ptr := 0
+		for i := 0; i < N; i++ {
+			if (rand.Float64() < overlap) {
+				e1.AddTransaction(txs[ptr])
+				e2.AddTransaction(txs[ptr])
+				txset1[*txs[ptr]] = struct{}{}
+				txset2[*txs[ptr]] = struct{}{}
+				ptr++
+			} else {
+				e1.AddTransaction(txs[ptr])
+				txset1[*txs[ptr]] = struct{}{}
+				ptr++
+				e2.AddTransaction(txs[ptr])
+				txset2[*txs[ptr]] = struct{}{}
+				ptr++
 			}
-			deliver1 := float64(N-len(txset1)) / float64(N)
-			deliver2 := float64(N-len(txset2)) / float64(N)
-			if deliver1 > 0.95 && deliver2 > 0.95 {
-				fmt.Printf("%.2f %.2f %.2f %.2f\n", r1, r2, deliver1, deliver2)
-				break
+			c1 += r1
+			c2 += r2
+			for c1 >= 1.0 {
+				c := e1.ProduceCodeword()
+				_, newtx := d1.AddCodeword(c)
+				for _, tx := range newtx {
+					delete(txset1, *tx)
+					delete(txset2, *tx)
+				}
+				c1 -= 1.0
 			}
+			for c2 >= 1.0 {
+				c := e2.ProduceCodeword()
+				_, newtx := d1.AddCodeword(c)
+				for _, tx := range newtx {
+					delete(txset1, *tx)
+					delete(txset2, *tx)
+				}
+				c2 -= 1.0
+			}
+		}
+		deliver1 := float64(N-len(txset1)) / float64(N)
+		deliver2 := float64(N-len(txset2)) / float64(N)
+		return deliver1, deliver2
+	}
+
+	minRate := (1.0-overlap) / 2.0
+	maxRate := 2.0
+	for r1 := minRate; r1 <= maxRate; r1 += 0.01 {
+		r2 := (minRate + maxRate/2)
+		lastOk := maxRate
+		lastFail := minRate
+		for lastOk - lastFail > 0.01 {
+			d1, d2 := test(r1, r2)
+			if d1 > threshold && d2 > threshold {
+				lastOk = r2
+				r2 = (r2 + lastFail) / 2.0
+			} else {
+				lastFail = r2
+				r2 = (r2 + lastOk) / 2.0
+			}
+		}
+		if lastOk != maxRate {
+			fmt.Printf("%.2f %.2f\n", r1, lastOk)
 		}
 	}
 }
 
 func main() {
 	fmt.Println("# rate1 rate2 deliver1 deliver2")
-	testOverlap(50, 10000, 0.8)
+	testOverlap(50, 10000, 0.8, 0.95)
 }
