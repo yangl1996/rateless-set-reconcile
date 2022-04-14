17651575f41ab28668e40bf8a803b1782a66a5fb
Lei Yang <lei@yangl1996.com>
2022-04-13 22:04:49 -0400

    update

---
+++ testbed/process.py
@@ -0,0 +1,82 @@
+#!/usr/local/bin/python3
+
+import re
+import argparse
+import glob
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("PREFIX", help="prefix of the files", type=str)
+parser.add_argument("-n", help="number of servers", type=int, default=19)
+#parser.add_argument("-d", "--delay", help="one-way per-edge delay in ms", type=float, default=70.0)
+#parser.add_argument("--name", help="name of the network", type=str, default="Test")
+#parser.add_argument("--latency", help="use latency file", type=str, default=None)
+args = parser.parse_args()
+
+results = []
+
+for i in range(args.n):
+    filename = args.PREFIX + "-" + str(i)
+    peercnt = 0
+    peers = {}
+    cwcnt = []
+    txcnt = []
+    gencnt = []
+    # take the first pass to find all peers
+    with open(filename) as f:
+        for line in f:
+            if "key exchanged with peer" in line:
+                start = line.find("with peer") + 10
+                end = line.find(",")
+                addr = line[start:end]
+                peers[addr] = peercnt
+                peercnt += 1
+                cwcnt.append([])
+    # take the second pass to gather data
+    with open(filename) as f:
+        started = False
+        for line in f:
+            if not started and "data logging warmup completed" in line:
+                started = True
+            if started:
+                if "received cws" in line:
+                    dt = int(datetime.strptime(line[0:19], '%Y/%m/%d %H:%M:%S').timestamp())
+                    start = line.find("peer") + 5
+                    end = line.find(" received")
+                    peeridx = peers[line[start:end]]
+                    start = line.find("cws") + 4
+                    end = line.find("\n")
+                    cnt = int(line[start:end])
+                    cwcnt[peeridx].append(cnt)
+                elif "tx=" in line:
+                    dt = int(datetime.strptime(line[0:19], '%Y/%m/%d %H:%M:%S').timestamp())
+                    start = line.find("tx=") + 3
+                    end = line.find(", p5_latency_ms")
+                    cnt = int(line[start:end])
+                    txcnt.append(cnt)
+                elif "generated tx" in line:
+                    dt = int(datetime.strptime(line[0:19], '%Y/%m/%d %H:%M:%S').timestamp())
+                    start = line.find("tx") + 3
+                    end = line.find("\n")
+                    cnt = int(line[start:end])
+                    gencnt.append(cnt)
+    # compute the total number of codewords
+    minLen = 10000000000
+    for i in range(peercnt):
+        if len(cwcnt[i]) < minLen:
+            minLen = len(cwcnt[i])
+    if len(txcnt) < minLen:
+        minLen = len(txcnt)
+    if len(gencnt) < minLen:
+        minLen = len(gencnt)
+    startCw = 0
+    endCw = 0
+    for i in range(peercnt):
+        startCw += cwcnt[i][0]
+        endCw += cwcnt[i][minLen-1]
+    startTx = txcnt[0]
+    endTx = txcnt[minLen-1]
+    startGen = gencnt[0]
+    endGen = gencnt[minLen-1]
+    # overhead (cw/tx), received tx rate
+    print(float(endCw-startCw) / float(endTx-startTx), float(endTx-startTx+endGen-startGen) / float(minLen))
