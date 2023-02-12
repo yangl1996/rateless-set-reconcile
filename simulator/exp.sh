3ed1295864095992424a4ad2c856fabe6ce8f72b
Lei Yang <lei@yangl1996.com>
2023-02-12 17:49:32 -0500

    log

--- simulator/exp.sh
+++ simulator/exp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-./simulator "$@" | sed -n 's/\([0-9]*.[0-9]*\) Node 1.*send rate \([0-9]*.[0-9]*\).*queue length \([0-9]*\).*/\1 \2 \3/p' > data.txt
+echo "#" "$@" > data.txt
+./simulator "$@" | sed -n 's/\([0-9]*.[0-9]*\) Node 1.*send rate \([0-9]*.[0-9]*\).*queue length \([0-9]*\).*/\1 \2 \3/p' >> data.txt
 gnuplot time-series.gnuplot
 
