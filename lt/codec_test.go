9ffa37ab179216c6589935edf1c4d94541e08d70
Lei Yang <lei@yangl1996.com>
2023-02-21 14:49:07 -0500

    added test for encoder

--- lt/codec_test.go
+++ lt/codec_test.go
@@ -2,12 +2,14 @@ package lt
 
 import (
 	"github.com/yangl1996/soliton"
+	"github.com/dchest/siphash"
 	"testing"
 	"encoding/binary"
 	"math/rand"
 )
 
 var testSalt = [SaltSize]byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+var hasher = siphash.New(testSalt[:])
 
 const simpleDataSize = 128
 type simpleData [simpleDataSize]byte
@@ -35,13 +37,17 @@ func (d *simpleData) Equals(t2 *simpleData) bool {
 	return true
 }
 
+func newSimpleData(i uint64) *simpleData {
+	data := simpleData{}
+	binary.LittleEndian.PutUint64(data[0:8], i)
+	return &data
+}
+
 func TestEncodeAndDecode(t *testing.T) {
 	dist := soliton.NewRobustSoliton(rand.New(rand.NewSource(0)), 500, 0.03, 0.5)
 	e := NewEncoder[*simpleData](testSalt, dist, 500)
 	for i := 0; i < 500; i++ {
-		data := simpleData{}
-		binary.LittleEndian.PutUint32(data[0:4], uint32(i))
-		tx := NewTransaction[*simpleData](&data)
+		tx := NewTransaction[*simpleData](newSimpleData(uint64(i)))
 		e.AddTransaction(tx)
 	}
 	dec := NewDecoder[*simpleData](testSalt, 1000)
---
+++ lt/encoder_test.go
@@ -0,0 +1,141 @@
+package lt
+
+import (
+	"github.com/yangl1996/soliton"
+	"testing"
+	"math/rand"
+	"bytes"
+)
+
+
+func TestReset(t *testing.T) {
+	dist1 := soliton.NewRobustSoliton(rand.New(rand.NewSource(0)), 500, 0.03, 0.5)
+	dist2 := soliton.NewRobustSoliton(rand.New(rand.NewSource(0)), 600, 0.03, 0.5)
+	e := NewEncoder[*simpleData](testSalt, dist1, 500)
+	for i := 0; i < 500; i++ {
+		tx := NewTransaction[*simpleData](newSimpleData(uint64(i)))
+		e.AddTransaction(tx)
+	}
+	e.Reset(dist2, 600)
+	if e.degreeDist != dist2 {
+		t.Error("incorrect degree distribution after resetting")
+	}
+	if len(e.window) != 0 {
+		t.Error("window not cleared after resetting")
+	}
+	if e.windowSize != 600 {
+		t.Error("incorrect window size after resetting")
+	}
+	if len(e.hashes) != 0 {
+		t.Error("existing transaction hashes not cleared after resetting")
+	}
+}
+
+func TestAddTransaction(t *testing.T) {
+	dist := soliton.NewRobustSoliton(rand.New(rand.NewSource(0)), 50, 0.03, 0.5)
+	e := NewEncoder[*simpleData](testSalt, dist, 50)
+	tx := NewTransaction[*simpleData](newSimpleData(uint64(1)))
+	if !e.AddTransaction(tx) {
+		t.Error("failed to add transaction when there is no conflict")
+	}
+	hasher.Reset()
+	hasher.Write(tx.Hash())
+	saltedHash := uint32(hasher.Sum64())
+	if len(e.window) != 1 {
+		t.Error("incorrect window after adding transaction")
+	}
+	if len(e.hashes) != 1 {
+		t.Error("incorrect existing transaction hashes after adding transaction")
+	}
+	added := e.window[0]
+	if added.saltedHash != saltedHash {
+		t.Error("added transaction has incorrect salted hash")
+	}
+	if !bytes.Equal(added.hash, tx.hash) {
+		t.Error("added transaction has incorrect hash")
+	}
+	if !bytes.Equal(added.data[:], tx.data[:]) {
+		t.Error("added transaction has incorrect data")
+	}
+	if _, there := e.hashes[saltedHash]; !there {
+		t.Error("salted hash of added transaction not present in existing transaction hashes")
+	}
+
+	if e.AddTransaction(tx) {
+		t.Error("successfully added duplicated transaction")
+	}
+
+	for i := 2; i < 51; i++ {
+		tx := NewTransaction[*simpleData](newSimpleData(uint64(i)))
+		if !e.AddTransaction(tx) {
+			t.Error("failed to add transaction when there is no conflict")
+		}
+	}
+
+	newTx := NewTransaction[*simpleData](newSimpleData(uint64(51)))
+	if !e.AddTransaction(newTx) {
+		t.Error("failed to add transaction when there is no conflict")
+	}
+	hasher.Reset()
+	hasher.Write(newTx.Hash())
+	newSaltedHash := uint32(hasher.Sum64())
+	if len(e.window) != 50 {
+		t.Error("incorrect size of window after adding transaction and flushing first added transaction")
+	}
+	if len(e.hashes) != 50 {
+		t.Error("incorrect size of existing transaction hashes after adding transaction and flushing first added transaction")
+	}
+	newAdded := e.window[49]
+	if newAdded.saltedHash != newSaltedHash {
+		t.Error("newAdded transaction has incorrect salted hash")
+	}
+	if !bytes.Equal(newAdded.hash, newTx.hash) {
+		t.Error("newAdded transaction has incorrect hash")
+	}
+	if !bytes.Equal(newAdded.data[:], newTx.data[:]) {
+		t.Error("newAdded transaction has incorrect data")
+	}
+	if _, there := e.hashes[newSaltedHash]; !there {
+		t.Error("salted hash of newAdded transaction not present in existing transaction hashes")
+	}
+}
+
+func TestProduceCodeword(t *testing.T) {
+	dist := soliton.NewRobustSoliton(rand.New(rand.NewSource(0)), 50, 0.03, 0.5)
+	e := NewEncoder[*simpleData](testSalt, dist, 50)
+	for i := 0; i < 50; i++ {
+		tx := NewTransaction[*simpleData](newSimpleData(uint64(i)))
+		e.AddTransaction(tx)
+	}
+	checkDataUnchanged := func() bool {
+		for i := 0; i < 50; i++ {
+			tx := NewTransaction[*simpleData](newSimpleData(uint64(i)))
+			if !bytes.Equal(e.window[i].data[:], tx.data[:]) {
+				return false
+			}
+		}
+		return true
+	}
+	for d := 1; d <= 50; d++ {
+		cw := e.produceCodeword(d)
+		if !checkDataUnchanged() {
+			t.Error("window content changed after producing codeword with degree", d)
+		}
+		sum := &simpleData{}
+		for _, saltedHash := range cw.members {
+			found := false
+			for _, stx := range e.window {
+				if stx.saltedHash == saltedHash {
+					found = true
+					sum = sum.XOR(stx.data)
+				}
+			}
+			if !found {
+				t.Error("member of codeword with degree", d, "not present in encoding window")
+			}
+		}
+		if !bytes.Equal(sum[:], cw.symbol[:]) {
+			t.Error("codeword with degree", d, "has incorrect symbol")
+		}
+	}
+}
