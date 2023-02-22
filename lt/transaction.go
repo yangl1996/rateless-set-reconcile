99d8ad7df0f3a475ef54c9f7c95d7423f3e0794e
Lei Yang <lei@yangl1996.com>
2023-02-21 22:29:41 -0500

    update

--- lt/transaction.go
+++ lt/transaction.go
@@ -12,7 +12,17 @@ type Transaction[T TransactionData[T]] struct {
 }
 
 func NewTransaction[T TransactionData[T]](data T) Transaction[T] {
-	return Transaction[T]{data, data.Hash()}
+	tx := Transaction[T]{}
+	// FIXME: this causes an allocation. We could have just take ownership of
+	// data passed to us as the argument, but in decodeCodewords in decoder.go
+	// we peel newly-decoded transaction from codewords it is blocking,
+	// including the one from which the transaction is decoded. If the
+	// transaction and the codeword use the same memory space (which will
+	// happen if we do not create new tx.data), we will XOR the data with
+	// itself and zero it out.  Remove the allocation after we fix this issue.
+	tx.data = tx.data.XOR(data)
+	tx.hash = tx.data.Hash()
+	return tx
 }
 
 func (t Transaction[T]) Data() T {
