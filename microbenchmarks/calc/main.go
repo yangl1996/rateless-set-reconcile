21227cee1f0a052cbc728001059f9702005d6401
Lei Yang <lei@yangl1996.com>
2023-04-20 22:30:24 -0400

    compute multiple senders

--- microbenchmarks/calc/main.go
+++ microbenchmarks/calc/main.go
@@ -5,26 +5,45 @@ import (
 	"github.com/yangl1996/rateless-set-reconcile/microbenchmarks"
 )
 
-func findMaxOverhead(blockSize int) (int, float64) {
-	maxOverhead := 0.0
-	maxOverheadOverlap := 0
-	for common := 0; common <= blockSize; common++ {
-		blk1 := microbenchmarks.SimulateOneSenderOverlap(100, blockSize, 0)
-		blk2 := microbenchmarks.SimulateOneSenderOverlap(100, blockSize, common)
-		overhead := float64(blk1 + blk2) / float64(blockSize + blockSize - common)
-		if overhead > maxOverhead {
-			maxOverhead = overhead
-			maxOverheadOverlap = common
+func findMaxOverhead(blockSize int, numNodes int) ([]float64, []int) {
+	totTx := blockSize
+	totCw := microbenchmarks.SimulateOneSenderOverlap(100, blockSize, 0)
+
+	maxOverhead := []float64{}
+	maximizingOverlap := []int{}
+	for node := 1; node < numNodes; node++ {
+		overhead := 0.0
+		overlap := 0
+		cw := 0
+		for common := 0; common <= blockSize; common++ {
+			blk := microbenchmarks.SimulateOneSenderOverlap(100, blockSize, common)
+			thisOverhead := float64(totCw + blk) / float64(totTx + blockSize - common)
+			if thisOverhead > overhead {
+				overhead = thisOverhead
+				overlap = common
+				cw = blk
+			}
 		}
+		maxOverhead = append(maxOverhead, overhead)
+		maximizingOverlap = append(maximizingOverlap, overlap)
+		totTx += blockSize - overlap
+		totCw += cw
 	}
-	return maxOverheadOverlap, maxOverhead
+	return maxOverhead, maximizingOverlap
 }
 
 func main() {
+	fmt.Println("# worst efficiency for 2 overlapping nodes at different block sizes")
 	fmt.Println("# block size   overhead upperbound   maximizing overlap")
 	blockSizes := []int{50, 100, 200, 500}
 	for _, bs := range blockSizes {
-		m, o := findMaxOverhead(bs)
-		fmt.Println(bs, o, m)
+		o, m := findMaxOverhead(bs, 2)
+		fmt.Println(bs, o[0], m[0])
+	}
+	fmt.Println("# efficiency for block size 100 as there are more overlapping nodes")
+	fmt.Println("# n-th node   overhead upperbound   maximizing overlap")
+	o, m := findMaxOverhead(100, 10)
+	for nodes := 2; nodes <= 10; nodes++ {
+		fmt.Println(nodes, o[nodes-2], m[nodes-2])
 	}
 }
