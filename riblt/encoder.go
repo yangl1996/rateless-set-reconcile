c3e65fe611600f6148031a95e4fde562a74f8185
Lei Yang <lei@yangl1996.com>
2023-06-25 21:36:25 -0400

    update

--- riblt/encoder.go
+++ riblt/encoder.go
@@ -7,8 +7,11 @@ import (
 const order = 18446744073709551557	// 2^64-59
 const order2 = 4294967291 // 2^32-5
 
+
 // TODO: prove that multiplying with a random number (mod prime number?) can replace keyed hash.
-// TODO: if we want hash, salt, and their multiplication to be in F_p (p is prime) then the fastest and simpliest way (I think?) is to let p be the largest prime number smaller than 2^32, and perform normal 64-bit multiplication hash*salt (result guaranteed to be within 2^64) and then take modulo. However we will be forced to use 4-byte hashes and salts. I think it is fine (we can use the per-peer salt trick everyone else is using) but might be good to see if there is a way to stick to 8-byte salts and hashes.
+// TODO: if we want hash, salt, and their multiplication to be in F_p (p is prime) then the fastest and simpliest way (I think?) is to let p be the largest prime number smaller than 2^32, and perform normal 64-bit multiplication hash*salt (result guaranteed to be within 2^64) and then take modulo. However we will be forced to use 4-byte hashes and salts. I think it is fine (we can use the per-peer salt trick everyone else is using) but might be good to see if there is a way to stick to 8-byte salts and hashes. A very simple solution is for the encoder to send conflicting transactions as-is when detected; since it happens very rare and each pair of peers can use a secret hash key, an adversary cannot forge too many conflicts.
+// TODO: replace siphash with xxhash (or whatever that supports native 4-byte output)
+// TODO: we can show that the inclusion/not of any two transactions is pairwise-independent. Is it enough?
 
 type CodedSymbol[T Symbol[T]] struct {
 	sum T
